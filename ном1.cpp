7efa198c52650a9a58f9609f2caadc47e9304452
valeriyamardanova <valeriyanoble@gmail.com>
2025-09-08 19:57:44 +0300

    Add files via upload

---
+++ ном1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float min, max, a, b, c, n = 3, sr, sum = 0;
+    cin >> a >> b >> c;
+    if (a >= b) {
+        max = a;
+        min = b;
+        if (c >= a) max = c;
+        else if (c <= b) min = c;
+    }
+    else {
+        max = b;
+        min = a;
+        if (c >= b) max = c;
+        else if (c <= a) min = c;
+    }
+    sum = a + b + c;
+    sr = sum / n;
+    cout << min << " " << sr << " " << max << "\n";
+    for (int i = 0; ; i++) {
+        cin >> a;
+        if (!a) break;
+        if (a < min) min = a;
+        if (a > max) max = a;
+        sum += a;
+        sr = sum / ++n;
+        cout << min << " " << sr << " " << max << "\n";
+    }
+    return 0;
+}
